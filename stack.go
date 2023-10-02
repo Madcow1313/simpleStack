d4f6180c407483c6b9c1edd62cf7c6edc790cb2c
Bulat <93150213+Madcow1313@users.noreply.github.com>
2023-10-02 18:57:56 +0300

    Create stack.go

---
+++ stack.go
@@ -0,0 +1,34 @@
+package Stack
+
+type Stack[T interface{}] []T
+
+//Check if stack is empty
+func (s *Stack[T]) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+//Push entity to stack
+func (s *Stack[T]) Push(n T) {
+	*s = append(*s, n)
+}
+
+//Checks if empty and returns top element and true if exists
+//or zero initialized value and false
+func (s *Stack[T]) Top() (T, bool) {
+	if s.IsEmpty() {
+		var t T
+		return t, false
+	}
+	return (*s)[len(*s)-1], true
+}
+
+//Pop and return top, true element if exists or zero initialized element, false
+func (s *Stack[T]) Pop() (T, bool) {
+	if s.IsEmpty() {
+		var t T
+		return t, false
+	}
+	el := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return el, true
+}
