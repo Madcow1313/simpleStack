d4972ffdad8b2026be49ef1b23c30bbe85520219
Madcow1313 <safbul@gmail.com>
2023-10-02 19:24:52 +0300

    docs?

--- stack.go
+++ stack.go
@@ -1,19 +1,20 @@
 package Stack
 
+/*Simple implementation of stack using generics*/
 type Stack[T interface{}] []T
 
-//Check if stack is empty
+// Check if stack is empty
 func (s *Stack[T]) IsEmpty() bool {
 	return len(*s) == 0
 }
 
-//Push entity to stack
+// Push entity to stack
 func (s *Stack[T]) Push(n T) {
 	*s = append(*s, n)
 }
 
-//Checks if empty and returns top element and true if exists
-//or zero initialized value and false
+// Checks if empty and returns top element and true if exists
+// or zero initialized value and false
 func (s *Stack[T]) Top() (T, bool) {
 	if s.IsEmpty() {
 		var t T
@@ -22,7 +23,7 @@ func (s *Stack[T]) Top() (T, bool) {
 	return (*s)[len(*s)-1], true
 }
 
-//Pop and return top, true element if exists or zero initialized element, false
+// Pop and return top, true element if exists or zero initialized element, false
 func (s *Stack[T]) Pop() (T, bool) {
 	if s.IsEmpty() {
 		var t T
